50e46fb6ef387473e849d4b31e77ee41fdd51d51
Brent Jackson <jxnblk@github.com>
2014-09-15 15:55:59 -0400

    Adjust variables

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
-module.exports = function(elements) {
+module.exports = function(elements, options) {
 
-  console.log('elements', elements.length);
+  var options = options || {};
+  var baseline = options.baseline || 16;
+  var paddingY = options.paddingY || 0;
 
   for (var i = 0; i < elements.length; i++) {
-    //var el = elements[i];
     var content = elements[i].textContent;
     var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
@@ -12,23 +13,25 @@ module.exports = function(elements) {
     var height;
 
     text.textContent = content;
-    text.setAttribute('y', '16');
+    text.setAttribute('y', baseline);
     text.setAttribute('font-family', 'inherit');
     text.setAttribute('font-size', '1rem');
     text.setAttribute('font-weight', 'inherit');
 
+    svg.setAttribute('width', '100%');
+    svg.setAttribute('max-height', '100%');
     for (var j = 0; j < elements[i].attributes.length; j++) {
       svg.setAttribute(elements[i].attributes[j].name, elements[i].attributes[j].value);
     }
+
     svg.appendChild(text);
     elements[i].parentNode.replaceChild(svg, elements[i]);
-    //elements[i].appendChild(svg);
 
     width = text.offsetWidth;
     height = text.offsetHeight;
 
-    svg.setAttribute('viewBox', '0 0 ' + width + ' ' + (height + 4));
-    svg.setAttribute('fill', 'tomato');
+    svg.setAttribute('viewBox', '0 0 ' + width + ' ' + (height + paddingY));
+    svg.setAttribute('fill', 'inherit');
 
   }
 
