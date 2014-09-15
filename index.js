0dde7272e9c14987bd68832969b27dcddc695885
Brent Jackson <jxnblk@github.com>
2014-09-15 18:16:30 -0400

    Adjusting things

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = function(elements, options) {
   var paddingY = options.paddingY || 0;
 
   for (var i = 0; i < elements.length; i++) {
+
     var content = elements[i].textContent;
     var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
@@ -20,10 +21,17 @@ module.exports = function(elements, options) {
 
     svg.setAttribute('width', '100%');
     svg.setAttribute('max-height', '100%');
+
     for (var j = 0; j < elements[i].attributes.length; j++) {
       svg.setAttribute(elements[i].attributes[j].name, elements[i].attributes[j].value);
     }
 
+    var rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    rect.setAttribute('width', '100%');
+    rect.setAttribute('height', '100%');
+    rect.setAttribute('fill', 'tomato');
+
+    svg.appendChild(rect);
     svg.appendChild(text);
     elements[i].parentNode.replaceChild(svg, elements[i]);
 
@@ -31,7 +39,7 @@ module.exports = function(elements, options) {
     height = text.offsetHeight;
 
     svg.setAttribute('viewBox', '0 0 ' + width + ' ' + (height + paddingY));
-    svg.setAttribute('fill', 'inherit');
+    svg.setAttribute('fill', 'currentcolor');
 
   }
 
