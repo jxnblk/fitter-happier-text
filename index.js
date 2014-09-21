463a7af0bcb4b077ccf22f317550e2fdfc7edf0e
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-09-21 15:33:55 -0700

    Added documentation to index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 module.exports = function(elements, options) {
 
+  /* Assign function arguments to variables.  The elements variable references the text
+  to be bound to the page.  Options is an optional argument that takes the form of an object with a
+  baseline and/or a paddingY */
   var options = options || {};
   var baseline = options.baseline || 16;
   var paddingY = options.paddingY || 0;
@@ -8,12 +11,14 @@ module.exports = function(elements, options) {
 
   for (var i = 0; i < elements.length; i++) {
 
+    // variable declarations
     var content = elements[i].textContent;
     var svg = doc.createElementNS('http://www.w3.org/2000/svg', 'svg');
     var text = doc.createElementNS('http://www.w3.org/2000/svg', 'text');
     var width;
     var height;
 
+    // assign proper styles and positioning to text elements
     text.textContent = content;
     text.setAttribute('y', baseline);
     text.setAttribute('font-family', 'inherit');
@@ -24,10 +29,12 @@ module.exports = function(elements, options) {
       svg.setAttribute(elements[i].attributes[j].name, elements[i].attributes[j].value);
     }
 
+    // set proper size, style, and fill for svg element
     svg.setAttribute('width', '100%');
     svg.setAttribute('style', 'max-height:100%;overflow-x:visible');
     svg.setAttribute('fill', 'currentcolor');
 
+    // append text element to page
     svg.appendChild(text);
     elements[i].parentNode.replaceChild(svg, elements[i]);
 
